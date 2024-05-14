907dc183a4cd2219020b333ea160a5bc9f541d20
slothGeorge <gosh20002@gmail.com>
2024-05-14 17:38:42 +0400

    feat: mirror dancer'sd direction and image

--- game/dancing_guy.py
+++ game/dancing_guy.py
@@ -12,8 +12,9 @@ class Dancer(pygame.sprite.Sprite):
         self.original = self.image
         self.stop_image = pygame.image.load(CURRENT_FOLDER / 'Pictures' / 'angry_dancer.png')
         self.mirrored_image_right = pygame.transform.flip(self.image, True, False)
-        self.mirrored_image_left = pygame.transform.flip(self.image, True, False)
+        self.mirrored_image_left = self.image
         self.previous_direction = 1
+        self.image_gen = self.image_generator()
 
         self.rect = self.image.get_rect()
         self.x_speed = 7
@@ -29,21 +30,30 @@ class Dancer(pygame.sprite.Sprite):
         self.scene = pygame.display.get_surface().get_rect()
         self.rect.centerx = self.scene.centerx
 
+    def image_generator(self):
+        if self.x_speed == 0:
+            self.x_speed = 7 * self.previous_direction
+        while True:
+            if self.previous_direction == 1:
+                yield self.mirrored_image_left
+            else:
+                yield self.mirrored_image_right
+
+    def mirror_image(self):
+        if (self.rect.left <= self.scene.left) and (self.x_speed < 0):
+            self.previous_direction = 1
+            self.x_speed *= -1
+            self.image = self.mirrored_image_left
+        elif (self.rect.right >= self.scene.right) and (self.x_speed > 0):
+            self.previous_direction = -1
+            self.x_speed *= -1
+            self.image = self.mirrored_image_right
+
     def update(self):
-        image = pygame.image.load(CURRENT_FOLDER / 'Pictures' / 'happy_dancer.png')
         if pygame.mixer.music.get_busy():
-            if self.image == self.stop_image:
-                self.image = image
-            if self.x_speed == 0:
-                self.x_speed = 7 * self.previous_direction
-            if (self.rect.left <= self.scene.left) and (self.x_speed < 0):
-                self.previous_direction = 1
-                self.x_speed *= -1
-                self.image = self.image = self.mirrored_image_left
-            elif (self.rect.right >= self.scene.right) and (self.x_speed > 0):
-                self.previous_direction = -1
-                self.x_speed *= -1
-                self.image = self.mirrored_image_left
+            self.image_gen = self.image_generator()
+            self.image = next(self.image_gen)
+            self.mirror_image()
             self.rect.x += self.x_speed
         else:
             self.image = self.stop_image
